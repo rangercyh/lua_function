3d0e6b5340994d7f6718cff22cc3da985254694e
caiyiheng <rangercyh@gmail.com>
2013-06-19 20:31:00 +0800

    a serialization of lua table

---
+++ lua_lib/TableFunctions.lua
@@ -0,0 +1,31 @@
+
+--[[
+打印lua中的table结构，按照table的结构进行缩进
+]]
+function print_lua_table (lua_table, indent)
+	indent = indent or 0
+	for k, v in pairs(lua_table) do
+		if type(k) == "string" then
+			k = string.format("%q", k)
+		end
+		local szSuffix = ""
+		if type(v) == "table" then
+			szSuffix = "{"
+		end
+		local szPrefix = string.rep("    ", indent)
+		formatting = szPrefix.."["..k.."]".." = "..szSuffix
+		if type(v) == "table" then
+			print(formatting)
+			print_lua_table(v, indent + 1)
+			print(szPrefix.."},")
+		else
+			local szValue = ""
+			if type(v) == "string" then
+				szValue = string.format("%q", v)
+			else
+				szValue = tostring(v)
+			end
+			print(formatting..szValue..",")
+		end
+	end
+end
